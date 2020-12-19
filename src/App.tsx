266bc79b6600e0da7312441c0f368123c6d7326b
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2020-12-19 12:28:09 -0300

    Adicionando mais um item ao navigationBottom

--- src/App.tsx
+++ src/App.tsx
@@ -47,11 +47,15 @@ const App: React.FC = () => (
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonLabel></IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon icon={ellipse} />
             <IonLabel>Tab 2</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab3" href="/tab3">
+            <IonIcon icon={square} />
+            <IonLabel>Tab 3</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon icon={square} />
