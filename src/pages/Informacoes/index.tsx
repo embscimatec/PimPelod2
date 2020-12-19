b9cefe925bb82a54f42da9f75f7390c81e6babfc
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2020-12-19 16:35:07 -0300

    Adicionando content com background na cor correta

--- src/pages/Informacoes/index.tsx
+++ src/pages/Informacoes/index.tsx
@@ -1,10 +1,14 @@
+import { IonContent, IonPage, IonText } from '@ionic/react';
 import React from 'react';
 
 const Informacoes: React.FC = () => {
 
     return (
         <>
-
+            <IonPage>
+                <IonContent color="primary">
+                </IonContent>
+            </IonPage>
         </>
     );
 };
