bfbac7f0a991dc814283c1082d72964f891f901d
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2020-12-19 16:49:54 -0300

    Adicionando titulo da pagina de informacoes

--- src/pages/Informacoes/index.tsx
+++ src/pages/Informacoes/index.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonPage, IonText } from '@ionic/react';
+import { IonCol, IonContent, IonGrid, IonPage, IonRow, IonText } from '@ionic/react';
 import React from 'react';
 
 const Informacoes: React.FC = () => {
@@ -6,7 +6,18 @@ const Informacoes: React.FC = () => {
     return (
         <>
             <IonPage>
-                <IonContent color="primary">
+                <IonContent color="primary" class="ion-padding">
+                    <IonGrid class="ion-margin">
+                        <IonCol class="ion-justify-content-center">
+                            <IonRow class="ion-justify-content-center">
+                                <IonText>
+                                    <h2>
+                                        Informações
+                                    </h2>
+                                </IonText>
+                            </IonRow>
+                        </IonCol>
+                    </IonGrid>
                 </IonContent>
             </IonPage>
         </>
