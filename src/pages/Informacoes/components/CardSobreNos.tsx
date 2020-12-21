80c129b64d193a8bd1f037089a0163d568460cc6
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2020-12-21 12:22:28 -0300

    Adicionando div com a classe do texto

--- src/pages/Informacoes/components/CardSobreNos.tsx
+++ src/pages/Informacoes/components/CardSobreNos.tsx
@@ -12,7 +12,17 @@ const CardSobreNos: React.FC = () => {
                         Sobre Nós
                     </h2>
                 </IonRow>
-               
+                <IonRow class="ion-margin-bottom">
+                    <h4>
+                        O que é o IEEE?
+                    </h4>
+                    <div className="texto">
+                        <h4>
+                            O Instituto de Engenheiros Eletricistas e Eletrônicos ou Instituto de Engenheiros Eletrotécnicos e Eletrónicos ou IEEE é uma organização profissional sem fins lucrativos. O IEEE é a maior organização profissional técnica do mundo dedicada ao avanço da tecnologia em benefício da humanidade.
+                        </h4>
+                    </div>
+                </IonRow>
+
             </IonCardContent>
         </IonCard>
     );
