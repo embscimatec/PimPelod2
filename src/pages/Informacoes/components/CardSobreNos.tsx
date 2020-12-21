f7657bcc40e4cc012be65c890fbad983695ebceb
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2020-12-21 11:33:29 -0300

    Adicionando título da parte das palavras chaves

--- src/pages/Informacoes/components/CardSobreNos.tsx
+++ src/pages/Informacoes/components/CardSobreNos.tsx
@@ -1,10 +1,23 @@
+import { IonCard, IonCardContent, IonRow } from '@ionic/react';
 import React from 'react';
 
 const CardSobreNos: React.FC = () => {
 
-    return(
-        <>
-        </>
+    return (
+        <IonCard>
+            <IonCardContent class="ion-margin-top">
+                <IonRow class="ion-margin-bottom">
+                    <h2>
+                        Palavras Chaves
+                    </h2>
+                </IonRow>
+                <IonRow class="ion-margin-bottom">
+                    <h4>
+                        Palavra: Descrição da palavra aaaaaaaaaaaaaaaaa
+                    </h4>
+                </IonRow>
+            </IonCardContent>
+        </IonCard>
     );
 };
 
