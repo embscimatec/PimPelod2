d5ab065dfa3696cd2f56c179d652cf7db52cc978
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2020-12-21 12:27:03 -0300

    Adicionando div com classe de titulo

--- src/pages/Informacoes/components/CardSobreNos.tsx
+++ src/pages/Informacoes/components/CardSobreNos.tsx
@@ -13,9 +13,11 @@ const CardSobreNos: React.FC = () => {
                     </h2>
                 </IonRow>
                 <IonRow class="ion-margin-bottom">
-                    <h4>
-                        O que é o IEEE?
-                    </h4>
+                    <div className="titulo">
+                        <h4>
+                            O que é o IEEE?
+                        </h4>
+                    </div>
                     <div className="texto">
                         <h4>
                             O Instituto de Engenheiros Eletricistas e Eletrônicos ou Instituto de Engenheiros Eletrotécnicos e Eletrónicos ou IEEE é uma organização profissional sem fins lucrativos. O IEEE é a maior organização profissional técnica do mundo dedicada ao avanço da tecnologia em benefício da humanidade.
