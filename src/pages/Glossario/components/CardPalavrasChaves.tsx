3f40013eabac6af162e9effb7f6e1c293aae81a7
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2020-12-22 17:36:19 -0300

    Corrigindo biblioteca de palavras

--- src/pages/Glossario/components/CardPalavrasChaves.tsx
+++ src/pages/Glossario/components/CardPalavrasChaves.tsx
@@ -11,35 +11,35 @@ const PalavrasChaves: React.FC = () => {
     const elementos = [
         {
             palavra: "Admissão Eletiva",
-            descricao: "a admissão pode demorar mais de 6 horas sem efeitos adversos."
+            descricao: "a admissão pode demorar mais de 6 horas sem efeitos adversos"
         },
         {
             palavra: "Circulação Extracorpórea",
-            descricao: "método o qual os pulmões e coração são substituídos temporariamente por uma máquina chamada “pulmão-coração artificial”."
+            descricao: "método o qual os pulmões e coração são substituídos temporariamente por uma máquina chamada “pulmão-coração artificial"
         },
         {
             palavra: "Contagem das Plaquetas",
-            descricao: "determina a quantidade de plaquetas no sangue, é fundamental para detecção, prevenção e tratamento de uma série de condições;"
+            descricao: "determina a quantidade de plaquetas no sangue, é fundamental para detecção, prevenção e tratamento de uma série de condições"
         },
         {
             palavra: "Contagem dos leucócitos",
-            descricao: "obter a quantidade de glóbulos brancos no sangue para evidenciar ou não, a presença de infecção ou inflamação;"
+            descricao: "obter a quantidade de glóbulos brancos no sangue para evidenciar ou não, a presença de infecção ou inflamação"
         },
         {
             palavra: "Creatina",
-            descricao: "taxa diretamente proporcional à massa muscular do paciente, quanto maior a massa muscular, maior a taxa;"
+            descricao: "taxa diretamente proporcional à massa muscular do paciente, quanto maior a massa muscular, maior a taxa"
         },
         {
             palavra: "Escala de coma de Glasglow",
             descricao: `avalia situações de trauma permitindo a identificação
                 de problemas neurológicos, a avaliação do nível de consciência da pessoa e ainda prever o
-                prognóstico;
+                prognóstico
             `
         },
         {
             palavra: "Excesso de base",
             descricao: `medido em m.mol/L, refere-se ao excesso ou déficit da quantidade
-            de base no sangue, indica se o sangue está em equilíbrio ácido-base.
+            de base no sangue, indica se o sangue está em equilíbrio ácido-base
             `
         },
         {
@@ -49,17 +49,17 @@ const PalavrasChaves: React.FC = () => {
         },
         {
             palavra: "Frequência Cardíaca",
-            descricao: `número de batimentos cardíacos por minuto.
+            descricao: `número de batimentos cardíacos por minuto
             `
         },
         {
             palavra: "Gasometria Arterial",
-            descricao: `Eficácia da troca de O entre os alvéolos e os capilares pulmonares, medida em mmHg de PaO .
+            descricao: `eficácia da troca de O entre os alvéolos e os capilares pulmonares, medida em mmHg de PaO
             `
         },
         {
             palavra: "Lactatemia",
-            descricao: `Concentração de ácido lático medida em mmol/L`
+            descricao: `concentração de ácido lático medida em mmol/L`
         },
         {
             palavra: "PaO (kPa)/FiO2",
@@ -67,53 +67,50 @@ const PalavrasChaves: React.FC = () => {
         },
         {
             palavra: "PaCO2",
-            descricao: `corresponde a pressão parcial de no sangue arterial e exprime a eficácia da ventilação alveolar`
+            descricao: `corresponde à pressão parcial de no sangue arterial e exprime a eficácia da ventilação alveolar`
         },
         {
             palavra: "PAS",
-            descricao: `Pressão arterial sistólica medida em mmHg, é o maior valor verificado durante a
-            aferição da medida da pressão arterial.`
+            descricao: `pressão arterial sistólica medida em mmHg. É o maior valor verificado durante a
+            aferição da medida da pressão arterial`
         },
         {
-            palavra: "Pós-cirúrgico ou operatório como razão de entrada na UTI",
+            palavra: "pós-cirúrgico ou operatório como razão de entrada na UTI",
             descricao: `indica se a causa da entrada do paciente na UTI foi devido a uma cirurgia ou procedimento ocorrido
             recentemente`
         },
         {
             palavra: "RFM",
-            descricao: `reação foto motora das pupilas.`
+            descricao: `reação foto motora das pupilas`
         },
         {
             palavra: "Ventilação Mecânica",
-            descricao: `Método de substituição da ventilação normal dos pulmões por
+            descricao: `método de substituição da ventilação normal dos pulmões por
             meio de um aparelho`
         }
     ];
 
     return (
-        <IonCard>
-            <IonCardContent class="ion-margin-top">
-                <IonRow class="ion-margin-bottom">
-                    <h2>
-                        Palavras Chaves
+        <>
+            <IonCard>
+                <IonCardContent class="ion-margin-top">
+            <IonRow class="ion-margin-bottom">
+                <h2>
+                    Palavras Chaves
                     </h2>
-                </IonRow>
-                {
-                    elementos.map((obj:PalavraChave) => {
-                       return <IonRow class="ion-margin-bottom">
-                            <h4>
-                                {obj.palavra}: {obj.descricao}
-                            </h4>
-                        </IonRow>
-                    })
-                }
-                {/* <IonRow class="ion-margin-bottom">
-                    <h4>
-                        Palavra: Descrição da palavra aaaaaaaaaaaaaaaaa
-                    </h4>
-                </IonRow> */}
+            </IonRow>
+            {
+                elementos.map((obj: PalavraChave) => {
+                    return <IonRow class="ion-margin-bottom">
+                        <p>
+                            {obj.palavra}: {obj.descricao}.
+                        </p>
+                    </IonRow>
+                })
+            }
             </IonCardContent>
         </IonCard>
+        </>
     );
 };
 
