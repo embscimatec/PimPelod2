22793834f7b3e75769db4c6fcc8190acb2d0cff6
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2020-12-22 17:07:10 -0300

    Alterando cor de fundo da página do glossário

--- src/pages/Glossario/index.tsx
+++ src/pages/Glossario/index.tsx
@@ -1,8 +1,13 @@
+import { IonPage, IonContent, IonGrid, IonRow, IonText, IonCol } from '@ionic/react';
 import React from 'react';
 
 const Glossario : React.FC = () => {
     return(
         <>
+        <IonPage>
+                <IonContent color="primary" >
+                </IonContent>
+            </IonPage>
         </>
     );
 };
