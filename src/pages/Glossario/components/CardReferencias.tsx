c80eb68979ab060b32e71c1f1170271a2d26d039
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2021-01-19 16:35:40 -0300

    Criando array de referencias e percorrendo-o na renderização

--- src/pages/Glossario/components/CardReferencias.tsx
+++ src/pages/Glossario/components/CardReferencias.tsx
@@ -9,14 +9,33 @@ interface Referencia {
 
 const Referencias: React.FC = () => {
 
+    const referencias = [
+        {
+            autor: "FARINA, Modesto; PEREZ, Clotilde; BASTOS, Dorinho",
+            titulo: "Psicodinâmica das cores em comunicação",
+            complemento: `Editora Blucher. 2011. pp. 13, 77, 86 e 96`
+        },
+    ];
+
     return (
-        <div className="textos" >
-            <IonRow class="ion-margin-top">
-                <h2>
-                    Referências
+        <>
+            <div className="textos" >
+                <IonRow class="ion-margin-top">
+                    <h2>
+                        Referências
                 </h2>
-            </IonRow>
-        </div>
+                </IonRow>
+            </div>
+            {
+                referencias.map((obj: Referencia) => {
+                    return <IonRow class="ion-margin-top">
+                        <p>
+                            {obj.autor}. <b>{obj.titulo}</b>. {obj.complemento}.
+                        </p>
+                    </IonRow>
+                })
+            }
+        </>
     );
 };
 
