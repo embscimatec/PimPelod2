d36c8c29e7b761a98fc636e4680dc3ca1302a2e8
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2021-01-19 16:24:08 -0300

    Adicionando título do CardReferenciasa

--- src/pages/Glossario/components/CardReferencias.tsx
+++ src/pages/Glossario/components/CardReferencias.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { IonRow } from '@ionic/react';
 
 const Referencias: React.FC = () => {
 
     return (
-        <>
-        </>
+        <div className="textos" >
+            <IonRow class="ion-margin-top">
+                <h2>
+                    Referências
+                </h2>
+            </IonRow>
+        </div>
     );
 };
 
