819ca8c63ad186272fe276747f6318863dc4c5e5
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2021-01-19 16:05:32 -0300

    Criando interface de conceito no CardConceitosAplicados

--- src/pages/Glossario/components/CardConceitosAplicados.tsx
+++ src/pages/Glossario/components/CardConceitosAplicados.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 
+interface Conceito {
+    nome: string,
+    descricao: string
+};
+
 const ConceitosAplicados: React.FC = () => {
+
+    const conceitos = [
+        {
+            nome: "XXXX",
+            descricao: "xxxxxxxxxxxxxxxxxxxx"
+        },
+        {
+            nome: "XXXX",
+            descricao: "xxxxxxxxxxxxxxxxxxxx"
+        },
+    ];
 
     return(
         <>
