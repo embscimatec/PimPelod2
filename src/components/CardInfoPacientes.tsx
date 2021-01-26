bfb6817192271f42a0c841c55654d2d8fb416e18
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2021-01-26 18:26:40 -0300

    Adicionando avatar e titulo do card ao CardInfoPaciente

--- src/components/CardInfoPacientes.tsx
+++ src/components/CardInfoPacientes.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { IonAvatar, IonCard, IonCardContent, IonIcon, IonImg, IonItem, IonRow } from '@ionic/react';
+import avatar_icon from '../assets/avatar_icon.svg';
 
 const InfoPaciente: React.FC = () => {
     return (
         <>
+            <IonCard>
+                <IonCardContent class="ion-margin-top">
+                    <IonItem>
+                        <IonAvatar slot="start">
+                            <img src={avatar_icon} />
+                        </IonAvatar>
+                        <div className="textos">
+                            <h2>Nome do Paciente</h2>
+                        </div>
+                    </IonItem>
+                </IonCardContent>
+            </IonCard>
         </>
     );
 };
