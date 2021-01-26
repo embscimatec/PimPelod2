85a45d7ee0eee27e87af8ac9e374f9a05fb933a3
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2021-01-26 17:47:34 -0300

    adicionando verificação de resultado ao CardProgressBar

--- src/components/CardProgressBar.tsx
+++ src/components/CardProgressBar.tsx
@@ -6,17 +6,27 @@ interface Valor {
 }
 
 const ProgressBar: FunctionComponent<Valor> = ( {porcentagem} : Valor ) => {
+
+    const statusVital = () => {
+        if (porcentagem <= 0.4)
+            return "success";
+        else if (porcentagem > 0.4 && porcentagem < 0.7)
+            return "warning";
+        else
+            return "dark";
+    }
+
     return (
         <>
             <IonCard>
                 <IonCardContent class="ion-margin-top">
                     <IonCol class="ion-justify-content-center">
-                        <IonText>
-                            <h1>{porcentagem*100} %</h1>
+                        <IonText color={statusVital()}>
+                            <h1>{porcentagem*100}%</h1>
                         </IonText>
 
                         <IonRow class="ion-margin-top">
-                            <IonProgressBar color="danger" value={porcentagem} />
+                            <IonProgressBar color={statusVital()} value={porcentagem} />
                         </IonRow>
                     </IonCol>
 
