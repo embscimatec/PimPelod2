7b3a526076cf3e72eab89a585627896054cea5f7
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2021-01-26 14:39:34 -0300

    criação do título da tela de resultados

--- src/pages/Resultado/index.tsx
+++ src/pages/Resultado/index.tsx
@@ -1,11 +1,26 @@
+import { IonCol, IonContent, IonGrid, IonPage, IonRow, IonText } from '@ionic/react';
 import React from 'react';
 
 const Resultado: React.FC = () => {
 
-    return(
-        <div>
-            
-        </div>
+    return (
+        <>
+            <IonPage>
+                <IonContent color="primary">
+                    <IonGrid class="ion-margin">
+                        <IonCol class="ion-justify-content-center">
+                            <IonRow class="ion-justify-content-center">
+                                <IonText>
+                                    <h2>
+                                        RESULTADO
+                                    </h2>
+                                </IonText>
+                            </IonRow>
+                        </IonCol>
+                    </IonGrid>
+                </IonContent>
+            </IonPage>
+        </>
     );
 };
 
