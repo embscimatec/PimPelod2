5238878072eaaa2b1596af15f4224219000bb4aa
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2021-01-26 15:35:44 -0300

    Adicionando Card e % ao CardProgressBar

--- src/components/CardProgressBar.tsx
+++ src/components/CardProgressBar.tsx
@@ -1,8 +1,16 @@
+import { IonCard, IonCardContent, IonText } from '@ionic/react';
 import React from 'react';
 
 const ProgressBar: React.FC = () => {
-    return(
+    return (
         <>
+            <IonCard>
+                <IonCardContent class="ion-margin-top">
+                    <IonText>
+                        <h1>90%</h1>
+                    </IonText>
+                </IonCardContent>
+            </IonCard>
         </>
     );
 };
