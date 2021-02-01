ade88493c8662d4b3159317b9e5fe451ff951f60
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2021-02-01 17:03:40 -0300

    Adicionando textos de situação e resultados do calculo ao card de informações do paciente

--- src/components/CardInfoPacientes.tsx
+++ src/components/CardInfoPacientes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonAvatar, IonCard, IonCardContent, IonIcon, IonImg, IonItem, IonRow } from '@ionic/react';
+import { IonAvatar, IonCard, IonCardContent, IonCol, IonIcon, IonImg, IonItem, IonRow } from '@ionic/react';
 import avatar_icon from '../assets/avatar_icon.svg';
 
 const InfoPaciente: React.FC = () => {
@@ -15,6 +15,26 @@ const InfoPaciente: React.FC = () => {
                             <h2>Nome do Paciente</h2>
                         </div>
                     </IonItem>
+                    <div className="dados">
+                        <IonRow>
+                            <IonCol>
+                                <p>
+                                    Situação:<br />
+                                    Método:<br />
+                                    Idade:<br />
+                                </p>
+                            </IonCol>
+                            <IonCol>
+                                <div className="info">
+                                    <p>
+                                        40%<br />
+                                        PIM<br />
+                                        19<br />
+                                    </p>
+                                </div>
+                            </IonCol>
+                        </IonRow>
+                    </div>
                 </IonCardContent>
             </IonCard>
         </>
