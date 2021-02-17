588326acd054ec7bb54547ee55fcf900a04d913b
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2021-02-17 16:52:00 -0300

    Adicionando mensagem no CardAviso

--- src/components/CardAvisos.tsx
+++ src/components/CardAvisos.tsx
@@ -1,10 +1,16 @@
-import { IonCard } from '@ionic/react';
+import { IonCard, IonCardContent } from '@ionic/react';
 import React from 'react';
 
 const CardAviso: React.FC = () => {
-    return(
+    return (
         <IonCard>
-            
+            <IonCardContent>
+                <div className="erros">
+                    <p>
+                        Não há resultados registrados!
+                    </p>
+                </div>
+            </IonCardContent>
         </IonCard>
     );
 };
