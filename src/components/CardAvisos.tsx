bbcd3904fb457ce7947f4b968bca893942fe85a0
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2021-02-17 16:41:18 -0300

    Criando componente CardAviso

---
+++ src/components/CardAvisos.tsx
@@ -0,0 +1,12 @@
+import { IonCard } from '@ionic/react';
+import React from 'react';
+
+const CardAviso: React.FC = () => {
+    return(
+        <IonCard>
+            
+        </IonCard>
+    );
+};
+
+export default CardAviso;
